1592621964f269de6b9293a3b5f11662a68832df
Nitin <nsinghboss02@gmail.com>
2021-12-30 00:21:06 +0530

    contains method used

--- src/com/codechef/Main.java
+++ src/com/codechef/Main.java
@@ -1,7 +1,6 @@
 package com.codechef;
 import java.util.ArrayList;
 import java.util.Scanner;
-import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
@@ -10,11 +9,14 @@ public class Main {
 	If you don't know the exact size then you can use something named as arraylist;
 	 */
         Scanner sc = new Scanner(System.in);
-        ArrayList <String> a1 = new ArrayList<>(6);
+        ArrayList <Integer > a1 = new ArrayList<>(6);
         // if you want to add the elements then:
         for(int i=0;i<3;i++){
-            a1.add(sc.next());
+            a1.add(sc.nextInt());
         }
         System.out.println(a1);
+        // if you want to check that the asked element is there in list is
+        System.out.println(a1.contains(69));
+        System.out.println(a1.contains(56));
     }
 }
